dbf7d5e81beb6d6c3a9756d823f044cbdd3e720e
brams17011983 <b.rams17011983@gmail.com>
2022-09-15 10:45:37 +0530

    First commit for CSE B evops project

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class Demo
+{
+static int i;
+static long l;
+static byte b;
+static boolean bo;
+static short s;
+static float f;
+static char c;
+static double d;
+public static void main(String args[])
+{
+System.out.println("int="+i);
+System.out.println("long="+l);
+System.out.println("byte="+b);
+System.out.println("bool="+bo);
+System.out.println("short="+s);
+System.out.println("float="+f);
+System.out.println("char="+c);
+System.out.println("double="+d);
+}
+}
